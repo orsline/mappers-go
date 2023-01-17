1c5b2e4d47ef852fa1ca1f225b44f95b41ff9480
zhangyanhua <zhangyanhua@huawei.com>
2023-01-17 16:36:48 +0800

    idmvs问题修改

--- mappers/idmvs/driver/driver.go
+++ mappers/idmvs/driver/driver.go
@@ -39,13 +39,14 @@ type IDMVS struct {
 	protocolConfig       IDMVSProtocolConfig
 	protocolCommonConfig IDMVSProtocolCommonConfig
 	visitorConfig        IDMVSVisitorConfig
-	listeners map[int]*IDMVSInstance
+	listeners            map[int]*IDMVSInstance
 }
 
 type IDMVSInstance struct {
-	server net.Listener
-	codeValue string
-	status bool
+	server       net.Listener
+	codeValue    string
+	status       bool
+	reportstatus bool
 }
 
 // InitDevice Sth that need to do in the first
@@ -64,39 +65,44 @@ func (d *IDMVS) InitDevice(protocolCommon []byte) (err error) {
 
 // SetConfig Parse the configmap's raw json message
 // In the case of high concurrency, d.mutex helps you get the correct value
-func (d *IDMVS) SetConfig(protocolCommon, visitor, protocol []byte) (port int,err error) {
+func (d *IDMVS) SetConfig(protocolCommon, visitor, protocol []byte) (port int, err error) {
 	d.mutex.Lock()
 	defer d.mutex.Unlock()
 	if protocolCommon != nil {
 		if err = json.Unmarshal(protocolCommon, &d.protocolCommonConfig); err != nil {
 			fmt.Printf("Unmarshal protocolCommonConfig error: %v\n", err)
-			return 0,err
+			return 0, err
 		}
 	}
 	if visitor != nil {
 		if err = json.Unmarshal(visitor, &d.visitorConfig); err != nil {
 			fmt.Printf("Unmarshal visitorConfig error: %v\n", err)
-			return 0,err
+			return 0, err
 		}
 
 	}
 	if protocol != nil {
 		if err = json.Unmarshal(protocol, &d.protocolConfig); err != nil {
 			fmt.Printf("Unmarshal protocolConfig error: %v\n", err)
-			return 0,err
+			return 0, err
 		}
 	}
-	return d.protocolCommonConfig.Port,nil
+	return d.protocolCommonConfig.Port, nil
 }
 
 // ReadDeviceData  is an interface that reads data from a specific device, data is a type of string
 func (d *IDMVS) ReadDeviceData(protocolCommon, visitor, protocol []byte) (data interface{}, err error) {
 	// Parse raw json message to get a IDMVS instance
-	port,err := d.SetConfig(protocolCommon, visitor, protocol)
+	port, err := d.SetConfig(protocolCommon, visitor, protocol)
 	if err != nil {
 		return nil, err
 	}
-	return d.listeners[port].codeValue,nil
+	if d.listeners[port].reportstatus == false {
+		return ""
+	}
+
+	d.listeners[port].reportstatus = false
+	return d.listeners[port].codeValue, nil
 }
 
 // WriteDeviceData is an interface that write data to a specific device, data's DataType is Consistent with configmap
@@ -112,48 +118,48 @@ func (d *IDMVS) StopDevice() (err error) {
 
 // GetDeviceStatus is an interface to get the device status true is OK , false is DISCONNECTED
 func (d *IDMVS) GetDeviceStatus(protocolCommon, visitor, protocol []byte) (status bool) {
-	port,err := d.SetConfig(protocolCommon, visitor, protocol)
-	if err != nil{
+	port, err := d.SetConfig(protocolCommon, visitor, protocol)
+	if err != nil {
 		return false
 	}
+
 	return d.listeners[port].status
 }
 
+func (d *IDMVS) connect() {
 
-func (d *IDMVS) connect(){
-
-	if d.listeners == nil{
+	if d.listeners == nil {
 		d.listeners = make(map[int]*IDMVSInstance)
 	}
 	clientPort := strconv.Itoa(d.protocolCommonConfig.Port)
-	address := fmt.Sprintf("0.0.0.0:%s",clientPort)
+	address := fmt.Sprintf("0.0.0.0:%s", clientPort)
 	d.listeners[d.protocolCommonConfig.Port] = new(IDMVSInstance)
 	var err error
 	d.listeners[d.protocolCommonConfig.Port].server, err = net.Listen("tcp", address)
 	if err != nil {
-		fmt.Printf("Listen %s server failed,err:%s\n",address,err)
+		fmt.Printf("Listen %s server failed,err:%s\n", address, err)
 		return
 	}
-	go func(){
-		conn,err := d.listeners[d.protocolCommonConfig.Port].server.Accept()
+	go func() {
+		conn, err := d.listeners[d.protocolCommonConfig.Port].server.Accept()
 		if err != nil {
-			fmt.Println("Listen.Accept failed,err:",err)
+			fmt.Println("Listen.Accept failed,err:", err)
 		}
 		defer conn.Close()
 		for {
-			buf := make([]byte,256)
-			n ,err := conn.Read(buf[:])
+			buf := make([]byte, 256)
+			n, err := conn.Read(buf[:])
 			if err != nil {
-				fmt.Println("Read from tcp server failed,err:",err)
+				fmt.Println("Read from tcp server failed,err:", err)
 				d.listeners[d.protocolCommonConfig.Port].status = false
-			}else{
+			} else {
 				data := buf[:n]
 				barCode := *(*string)(unsafe.Pointer(&data))
-				barCode = strings.TrimRight(strings.TrimLeft(barCode,"<p>"),"</p>")
+				barCode = strings.TrimRight(strings.TrimLeft(barCode, "<p>"), "</p>")
 				d.listeners[d.protocolCommonConfig.Port].codeValue = barCode
 				d.listeners[d.protocolCommonConfig.Port].status = true
+				d.listeners[d.protocolCommonConfig.Port].reportstatus = true
 			}
-
 		}
 	}()
 }
