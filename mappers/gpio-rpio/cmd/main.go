de807da2e84b8a67abd9899b860c777e97d85aac
zhangyanhua <zhangyanhua@huawei.com>
2023-02-10 20:32:32 +0800

    sync main branch

--- mappers/gpio-rpio/cmd/main.go
+++ mappers/gpio-rpio/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/kubeedge/mappers-go/mapper-sdk-go/pkg/service"
-	"github.com/kubeedge/mappers-go/mappers/gpio/driver"
+	"github.com/kubeedge/mappers-go/mappers/gpio-rpio/driver"
 )
 
 // main gpio device program entry
