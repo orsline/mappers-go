c1b4bcf08cd1f1ad2091774aeca496936c2e715e
zhangyanhua <zhangyanhua@huawei.com>
2023-02-13 21:55:37 +0800

    合入gpio修改

--- mappers/gpio-atlas200/driver/gpio.go
+++ mappers/gpio-atlas200/driver/gpio.go
@@ -225,7 +225,7 @@ func isPca6416Pin(pin Pin) bool {
 
 // IOCTL send ioctl
 func IOCTL(fd *os.File, name, data uintptr) error {
-	_, _, err := syscall.SyscallN(syscall.SYS_IOCTL, fd, name, data)
+	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, name, data)
 	if err != 0 {
 		return syscall.Errno(err)
 	}
